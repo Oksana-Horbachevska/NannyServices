61a638dc77a4687616382863d66c5607a6d31932
Oksana-Horbachevska <oksanapaolo@gmail.com>
2026-01-20 22:25:31 +0100

    fix build bags

--- src/components/NannyCard/NannyCard.tsx
+++ src/components/NannyCard/NannyCard.tsx
@@ -10,7 +10,7 @@ import toast from "react-hot-toast";
 
 interface Props {
   nanny: Nanny;
-  onToggleFavorite: () => void;
+  onToggleFavorite?: () => void;
 }
 
 export default function NannyCard({ nanny, onToggleFavorite }: Props) {
